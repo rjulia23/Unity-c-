4a74f0403d9f3357df49b3026d2b43520de727bc
rjulia23 <47512918+rjulia23@users.noreply.github.com>
2019-03-07 23:11:10 -0800

    learning C# and Unity

---
+++ Number Wizard UI/Number Wizard UI/Assets/NumberWizard.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using TMPro;
+
+public class NumberWizard : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI computerGuess;
+
+    private int max = 1000;
+    private int min = 1;
+    private int guess;
+
+    void Start(){
+        StartGame();
+    }
+
+    private void NewGuess(){
+        System.Random rnd = new System.Random();
+        guess = rnd.Next(min, max);
+        computerGuess.text = "Is your number " + guess + "?";
+    }
+
+    private void StartGame(){
+        NewGuess();;
+    }
+    public void OnPressHigher(){
+        min = guess + 1;
+        NewGuess();
+    }
+    public void OnPressLower(){
+        max = guess - 1;
+        NewGuess();
+    }
+}
